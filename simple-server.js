3ad190aa04dbbff0f23b0a014242bb7309b65260
hazhazbaby <sofihazel14@gmail.com>
2025-07-03 16:04:25 -0600

    Add simple-server.js 1751580266394

---
+++ simple-server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile('index.html', (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+});
+
+const PORT = 8000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
